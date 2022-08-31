7441ba2ad306ee89eaeaef907a8518eed907402e
504 <504@DESKTOP-8Q3NP2T>
2022-08-31 18:15:07 +0900

    asd

--- front2/public/CSS/웹디자인/chapter4/form.js
+++ front2/public/CSS/웹디자인/chapter4/form.js
@@ -1,3 +1,27 @@
 /**
  * 
  */
+ 
+
+
+let applicant_list = []
+
+
+submit.addEventListener('click',(event)=>{
+	let name = document.querySelector("#name").value
+	let eng_name = document.querySelector("#eng_name").value
+	let email = document.querySelector("#email").value
+	let portpolio = document.querySelector("#portpolio").value
+	
+	let applicant = {
+		이름:name,
+		영어이름:eng_name,
+		이메일: email,
+		포폴:portpolio
+		
+	}
+	
+	
+	console.log(applicant)
+	
+})
--- front2/public/JS/함수/엘레베이터.js
+++ front2/public/JS/함수/엘레베이터.js
@@ -1,5 +1,21 @@
-let 상승버튼 = [null,null,null,null,null]
-let 하강버튼 = [null,null,null,null,null]
-let 내부버튼 = [null,null,null,null,null,null]
+let 상승버튼 = [false,false,false,false]
+let 하강버튼 = [false,false,false,false]
+let 내부버튼 = [false,false,false,false,false]
 let e_f = Math.floor( (Math.random()*5)+1 )
 document.getElementById('elevator').innerHTML = "엘레베이터 층수 : "+e_f
+
+
+
+function downbutton (i){
+	if(!상승버튼.includes(true)&&!하강버튼.includes(true)&&!내부버튼.includes(true)){
+		하강()
+	}
+	else if ()
+	
+	하강버튼[i-2]=true
+	
+	
+	
+	
+	
+}
