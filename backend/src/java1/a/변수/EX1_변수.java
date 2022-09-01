46dc6164df4893078ad4452213c89fa1e31ac276
504 <504@DESKTOP-8Q3NP2T>
2022-09-01 12:26:50 +0900

    asd

---
+++ backend/src/java1/a/변수/EX1_변수.java
@@ -0,0 +1,36 @@
+package java1.a.변수;
+
+public class EX1_변수 {
+	public static void main(String[] args) {
+		int value = 30;
+		int result = value + 10 ;
+		System.out.println(result);
+		
+		int hour = 3;
+		int minute = 5;
+		System.out.println(hour+"시간"+minute+"분");
+		
+		int totalMinute = (hour * 60 )+ minute;
+		System.out.println(totalMinute);
+		
+		
+		int x = 3;
+		int y = 5;
+		System.out.println("x: "+x+",y: "+y);
+		
+		int temp = x;
+		x = y ;
+		y = temp;
+		System.out.println("x: "+x+",y: "+y);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
