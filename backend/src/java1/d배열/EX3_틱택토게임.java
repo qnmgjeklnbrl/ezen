70c62b1c665c63f7c30892ca97acaa6ad20b263d
504 <504@DESKTOP-8Q3NP2T>
2022-09-06 16:39:37 +0900

    qwe

--- backend/src/java1/d배열/EX3_틱택토게임.java
+++ backend/src/java1/d배열/EX3_틱택토게임.java
@@ -2,95 +2,92 @@ package java1.d배열;
 
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Arrays;
+
 public class EX3_틱택토게임 {
 	public static void main(String[] args) {
-		
+		// * String(문자) 9개 를 저장할수 있는 배열 선언과 초기값[처음값]
+		String[ ] 게임판 = { "[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]" };
 		Scanner scanner = new Scanner(System.in);
-		String 승리 = " ";
-		boolean 게임종료 = true;
-		String[] 게임판 = {"[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]"};
-		
-		
-		for(int i = 0 ; i<=8 ; i++) {
-			System.out.print(게임판[i]);
-			if(i==2 ||i==5) {
-				System.out.println();
-			}
+		boolean game = true;
+		int count = 0;
+		while (game) { // 무한루프 [ 종료조건 : 승리판단 있을경우 ] // while 1 
 			
-				
-		}
-		while(게임종료){
-			System.out.println("\n위치를 입력해주세요."); int 사용자 = scanner.nextInt();
-			if(!게임판[사용자].equals("[ ]")) {
-				System.out.println("이미 선택된 위치 입니다. 다시 선택해주세요.");
-			}else {
-				게임판[사용자] = " O ";
-			}
-			while(true) {
-				Random random =new Random();
-				int 컴퓨터 = random.nextInt(9);
-				if(게임판[컴퓨터].equals("[ ]")){
-					게임판[컴퓨터]=" X ";	
+			// 1. 게임판 출력 - 배열내 모든 데이터 출력 
+			for( int i = 0 ; i<게임판.length ; i++ ) { // i는 0부터 마지막인덱스( 배열의길이-1 ) 까지 1씩 증가
+				System.out.print( 게임판[i] );
+				// 1-2 3줄[ 인덱스 : 2 , 5 , 8 ] 마다 줄바꿈
+				if( i == 2 || i == 5 || i == 8 ) { System.out.println(); }
+				// if( i == 2 || i == 5 || i == 8 ) ---> if( i % 3 == 2 )
+			} // for end 
+			
+			// 2. 사용자 에게 위치 입력받기 
+			while( true ) { // 무한루프 [ 종료조건 : 알을 두었을때 종료 break ] // while 2
+				System.out.print("안내) 위치 선택 : "); int 위치 = scanner.nextInt();
+				if( 위치 < 0 || 위치 > 8 ) { System.out.println("안내) 선택할수 없는 번호입니다.");}
+				// 해당 위치에 알이 없으면 해당 위치에 알두기 
+				if( 게임판[위치].equals("[ ]") ) {  게임판[위치] = "[O]"; break; } // 알을 두면 무한루프 종료
+				else { System.out.println("안내) 이미 알이 존재하는 위치 입니다. "); }
+			} // while 2 end 
+			
+			// 3. 컴퓨터 에게 위치 난수 생성
+			while( true ) { // while 2
+				if(count==4) {
 					break;
 				}
-			}
-			
-			
-			
-			for(int i = 0 ; i<=8 ; i++) {
-				System.out.print(게임판[i]);
-				if(i==2 ||i==5) {
-					System.out.println();
+				Random random = new Random(); // 랜덤 객체 생성 
+				int 위치 = random.nextInt(9); // 0~8 난수 생성 
+				if( 게임판[위치].equals("[ ]")){ 게임판[위치] = "[X]";count++; break; } // 알을 두면 무한루프 종료
+			} // while 2 end 
+			for(int i =0 ; i<=6 ; i+=3) {//가로로 이기는 경우
+				if(!게임판[i].equals("[ ]") && 게임판[i].equals(게임판[i+1]) && 게임판[i+1].equals(게임판[i+2]) ) {
+					System.out.println("게임종료) 승리: "+게임판[i]); game = false;
 				}
-				
-					
-			}
-		
-		for(int i = 0 ; i<=6 ;i=i+3) {
-			if(!게임판[i].equals("[ ]")&&게임판[i].equals(게임판[i+1])&&게임판[i+1].equals(게임판[i+2])) {
-				승리=게임판[i];	System.out.println("게임종료)승리: "+게임판[i]);  게임종료 = false;
-			}
-		}
-		for(int i = 0 ;  i<=2 ; i++) {
-			if(!게임판[i].equals("[ ]") && 게임판[i] == 게임판[i+3] && 게임판[i+3] == 게임판[i+6] ) {
-				승리=게임판[i];	System.out.println("게임종료)승리: "+게임판[i]);  게임종료 = false;
-			}
-		}
-		for(int i = 0 ; i<=2 ; i=i+2 ) {
-			if(i==0&&!게임판[i].equals("[ ]") && 게임판[i] == 게임판[i+4] && 게임판[i+4] == 게임판[i+8] ) {
-				승리=게임판[i];	System.out.println("게임종료)승리: "+게임판[i]);  게임종료 = false;
-			}else if(i==2&&!게임판[i].equals("[ ]") && 게임판[i] == 게임판[i+2] && 게임판[i+2] == 게임판[i+4] ) {
-				승리=게임판[i];	System.out.println("게임종료)승리: "+게임판[i]);  게임종료 = false;
 			}
 			
-		}
-		if(!Arrays.asList(게임판).contains("[ ]")) {
-			System.out.println("무승부입니다.");
-			게임종료 = false;
-		}
-		
-		
-	   }
-		
-		
-	}	
-	
-}
-
-
-
-
-
-
-
-
+			for(int i =0 ; i<=2 ; i++) {//세로로 이기는 경우
+				if(!게임판[i].equals("[ ]") && 게임판[i].equals(게임판[i+3]) && 게임판[i+3].equals(게임판[i+6]) ) {
+					System.out.println("게임종료) 승리: "+게임판[i]); game =false;
+				}
+			}
+			
+			for(int i = 0 ; i<=2 ; i=i+2 ){//대각선으로 이기는 경우
+				if( i==0 && !게임판[i].equals("[ ]") && 게임판[i].equals(게임판[i+4]) && 게임판[i+4].equals(게임판[i+8])){
+					System.out.println("게임종료) 승리: "+게임판[i]); game = false;
+				}
+				else if( i==2 && !게임판[i].equals("[ ]") && 게임판[i].equals(게임판[i+2]) && 게임판[i+2].equals(게임판[i+4])){
+					System.out.println("게임종료) 승리: "+게임판[i]); game = false;
+				} 
+			}
+			int count1 = 0;
+			for(int i = 0 ; i<=8 ; i++) {//무승부
+				
+				if(!게임판[i].equals("[ ]")) {
+					count1++;
+					if(count1==9 && game==true) {
+					 System.out.println("무승부입니다.");game =false;
+					}
+				}
+			}
+			
+			// 4. 승리 판단 [ 과제 ] 
+			
+		} // while 1  end 
+	} // main end 
+} // class end 
 /*
- * 틱택토 게임 [조건 ] 1.게임판 9칸 사용 -> 배열 2.사용자가 알을 두고자 하는 위치 [인덱스] 입력 3.컴퓨터가 난수 생성
- * [0~8] 1. 난수의 위차가 이미 다른 알이 존재하면 재생성 [중복제거] 4.승리자 판단 [1.가로로 이기는수  2.세로로 이기는수   3.무승부로 이기는수]
- * 
- * [출력] 
- * [ ] [ ] [ ]  
- * [ ] [ ] [ ]
- * [ ] [ ] [ ]
+ 	틱택토 게임 설계
+ 		[ 조건 ]
+ 		1. 게임판 9칸 사용 -> 배열[0~8] 
+ 			1. String 배열		클래스는 비교연산자 불가능
+ 				==  <---> 문자열1.equals(문자열2)
+ 				!=  <---> !문자열1.equals(문자열2)
+ 		2. 사용자[O]가 알을 두고자 하는 위치[인덱스] 입력[0~8]
+ 			1. 선택된 위치가 이미 다른 알이 존재하면 재입력 [중복제거]
+ 		3. 컴퓨터[X]가 난수생성[0~8]
+ 			1. 난수의 위치가 이미 다르 알이 존재하면 재생성 [ 중복제거 ]
+ 		4. 승리자 판단 [ 1.가로로 이기는수 2. 세로로 이기는수 3. 대각선으로 이기는수 4.무승부 ]
+ 		[ 출력 ] 게임판 출력 예시 
+ 			[ ] [ ] [ ] 
+ 			[ ] [ ] [ ] 
+ 			[ ] [ ] [ ] 
  */
