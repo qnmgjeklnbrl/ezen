e9804d95d24170e8c5cf603ef45949530e38ed2c
504 <504@DESKTOP-8Q3NP2T>
2022-09-07 10:36:36 +0900

    asd

--- backend/src/java1/d배열/EX7_비회원제게시판.java
+++ backend/src/java1/d배열/EX7_비회원제게시판.java
@@ -19,7 +19,7 @@ public class EX7_비회원제게시판 {
 			System.out.println("1.글쓰기      2.글보기");
 			System.out.print("선택 : "); int btn = scanner.nextInt();
 			if(btn==1) {//글쓰기
-				
+				System.out.println("==============글 쓰기 페이지 ==============");
 				System.out.print("title: ");String title = scanner.next();
 				System.out.print("content: ");String content = scanner.next();
 				System.out.print("writer: "); String writer = scanner.next();
@@ -47,10 +47,70 @@ public class EX7_비회원제게시판 {
 				
 				
 			}else if(btn==2) {
+				int index;
+				if(boardList[0][0]!=null) {
+						
+					
+				
+					System.out.println("=================게시물 상세 페이지==================");
+					System.out.print("게시물번호(인덱스) 선택:  "); index = scanner.nextInt();
+					if(boardList[index][0]!=null) {
+						System.out.println("작성자: "+boardList[index][3]+"  제목: "+boardList[index][0]);
+						System.out.println("내용: "+boardList[index][1] );
+				
+						System.out.println("===================================================");
+						System.out.println("1. 목록보기(뒤로가기)       2.글삭제               3.글수정");
+						System.out.print("선택: "); int btn2 = scanner.nextInt();
+				
+						if(btn2==1) {
+					
+					
+						}else if(btn2==2) {
+							System.out.print("알림)현 게시물 비밀번호: "); String pw = scanner.next();
+							if(boardList[index][3].equals(pw)) {
+								boardList[index][0]=null;
+								boardList[index][1]=null;
+								boardList[index][2]=null;
+								boardList[index][3]=null;
+								for( int i = index ; i<boardList.length ; i++  ) {
+									boardList[i][0] = boardList[i+1][0];
+									boardList[i][1] = boardList[i+1][1];
+									boardList[i][2] = boardList[i+1][2];
+									boardList[i][3] = boardList[i+1][3];
+							
+									if( boardList[i+1][0] == null ) break; 
+								} 
+								System.out.println("알림) 삭제 성공");
+							}else {System.out.println("알림) 비밀번호가 다릅니다.");}
+						
+						
+						
+						
+						
+						
+					
+						}else if(btn2==3) {
+							System.out.print("알림)현 게시물 비밀번호: "); String pw = scanner.next();
+							if(boardList[index][3].equals(pw)) {
+								System.out.print("수정할 제목 : "); boardList[index][0]=scanner.next();
+								System.out.print("수정할 내용 : "); boardList[index][1]=scanner.next();
+							}else {
+								System.out.println("알림) 비밀번호가 다릅니다.");
+							}
+						
+						
+						
+					
+						}else {
+							System.out.println("알 수 없는 번호입니다. 다시입력해주세요.");
+						}
+					}else {System.out.println("게시물이 존재하지 않습니다.");}
+				
+				}else {System.out.println("게시물이 존재하지 않습니다.");}
 				
 				
 			}else {
-				System.out.println("다시입력해주세요.");
+				System.out.println("알 수 없는 번호입니다. 다시입력해주세요.");
 				
 			}
 		
