c7c43d7a99bdab7145845afbd146218bf1b60503
504 <504@DESKTOP-8KB80JB>
2022-09-20 17:50:28 +0900

    qwe

--- backend/src/java1/e클래스/EX16_싱글톤/Simgleton.java
+++ backend/src/java1/e클래스/EX16_싱글톤/Simgleton.java
@@ -1,5 +1,18 @@
 package java1.e클래스.EX16_싱글톤;
 
 public class Simgleton {
-
+	// 싱글톤 : 객체 1개만 선언 가능한 클래스
+		//방법 : 외부에서 생성자를 못쓰게 하자
+		//private : 현재 클래스에서만 호츨 가능
+		//public : 프로젝트 어디서든 호출 가능
+	
+	private Simgleton() {
+		
+		
+	}
+	
+	
+	
+	
+  static final  double  EARTH_RADIUS = 6400;
 }
